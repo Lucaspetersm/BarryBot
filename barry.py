b819b4c7e271fe22db149955e868c1bd3b7b3991
Gareth Welch <garethpeterw@hotmail.co.uk>
2017-03-04 20:15:10 +0000

    2.0dev2
    - sub_list.txt -> nsfw_sub_list.txt
    - Added missing furry_sub_list.txt
    
    barry.py
    - Cleaned up code some more
    - Added `ver` and `user_agent` constants
    - Most of command.response code now moved to commands.py
    
    commands.py
    - Some code clean-up
    - All implemented commands and responses now moved
    - `filter_links` function added to filter out self posts and low score
    links
    
    launch.py
    - Fully ported to Python 3 now
    - Code cleaned up significantly. (Just needs comments now!)
    - Added optional module functionality
    - Now attempts to restart crashed programs on its own
    - `launch_program` variable added for simpler configuration

--- barry.py
+++ barry.py
@@ -1,5 +1,5 @@
 '''
-    Barry Bot 2.0dev1
+    Barry Bot 2.0dev2
     
     A pretty nutty Discord bot.
     
@@ -9,10 +9,14 @@
     Licensed under GNU General Public License v3.
 '''
 
-import discord, praw, random, string, time, youtube_dl
+import platform, random, string, time
+import discord, praw, youtube_dl
 import commands, config
 from discord import opus
-from sys import exit as sysexit
+
+ver = "2.0dev2"
+
+user_agent = platform.system().lower() + ":pw.yalnix.barrybot:" + ver + " by /u/Yalnix"
 
 OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
 
@@ -28,7 +32,7 @@ def load_opus_lib(opus_libs=OPUS_LIBS):
         else:
             raise RuntimeError('Could not load an opus lib. Tried %s' % (', '.join(opus_libs)))
 
-load_opus_lib()
+#load_opus_lib()
 
 if discord.opus.is_loaded():
   print("Loaded Opus")
@@ -36,99 +40,20 @@ else:
   print("Opus has not been loaded")
 
 client = discord.Client()
-general = discord.Client(id="228134125003866113")
+general = discord.Client(id="228134125003866113") #General voice channel
 
-r = praw.Reddit("Pull V1.0 by /u/Yalnix")
-r.login(config.reddit_user, config.reddit_pass, disable_warning=True)
+reddit = praw.Reddit(user_agent)
+reddit.login(config.reddit_user, config.reddit_pass, disable_warning=True)
 
 @client.event
 async def on_message(message):
-    channel = message.channel
     if message.author == client.user:
         return
+    else:
+        commands.on_message(discord, reddit, message)
 
-    if "😂" in message.content:
-        msg = (':joy:'*400).format(message)
-        await client.send_message(message.channel, msg)
-    if "right there" in message.content.lower():
-        msg = ":ok_hand::ok_hand::ok_hand: right:heavy_check_mark:there :heavy_check_mark::heavy_check_mark:if i do ƽaү so my self :100: i say so :100: thats what im talking about right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ:100: :ok_hand::ok_hand: :ok_hand:НO0ОଠOOOOOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ:ok_hand: :ok_hand::ok_hand: :ok_hand: :100: :ok_hand: :eyes: :eyes: :eyes: :ok_hand::ok_hand:Good shit"
-        await client.send_message(message.channel, msg)
-    if "Hello!" in message.content:
-        msg = ("Fuck you faggot")
-        await client.send_message(message.channel, msg)
-    if "boi" in message.content.lower():
-        msg = "i"*1000
-        await client.send_message(message.channel, msg)
-    if "What time is it?" in message.content:
-        await client.send_message(message.channel, "It's high noon!")
-                  
-    if message.content.startswith('!getmeme'):
-        import random
-        global myline
-        submissions = r.get_subreddit("dankmemes").get_hot(limit=100)
-        sub_list = []
-        for submission in submissions:
-            sub_list.append(submission)
-        url2 = random.choice(sub_list)
-        url = url2.url
-        if "www.reddit.com" not in url:
-          await client.send_message(message.channel, "Initializing Dank MayMays \n\nMayMay Level: `[----____]` 25% \n\nMayMay Level: `[------__]` 50% \n\nMayMay Level: `[--------]` 100% \n\nCommencing Meme: " + url)
-        else:
-          print("Self-Post")
-    
-    if message.content.startswith('!getporn'):
-        import random
-        global myline
-        lines = open('sub_list.txt').read().splitlines()
-        myline = random.choice(lines)
-        print(myline)
-        sub_list = []
-        submissions = r.get_subreddit(myline).get_top_from_all(limit=5)
-        for submission in submissions:
-            sub_list.append(submission)
-        url2 = random.choice(sub_list)
-        url = url2.url
-        await client.send_message(message.channel, url)
-                
-    if message.content.startswith('!getdick'):
-        import random
-        global myline
-        submissions = r.get_subreddit("dickpics").get_hot(limit=100)
-        sub_list = []
-        for submission in submissions:
-            sub_list.append(submission)
-        url2 = random.choice(sub_list)
-        url = url2.url
-        if "www.reddit.com" not in url:
-          await client.send_message(message.channel, "Initializing Dank Dicks \n\nMayMay Level: `[----____]` 25% \n\nDick Level: `[------__]` 50% \n\nDick Level: `[--------]` 100% \n\nCommencing Dick: " + url)
-        else:
-          print("Self-Post")
-                    
-    if message.content.startswith('!getfurryporn'):
-        import random
-        global myline
-        lines = open('furry_sub_list.txt').read().splitlines()
-        myline = random.choice(lines)
-        print(myline)
-        sub_list = []
-        submissions = r.get_subreddit(myline).get_top_from_all(limit=500)
-        for submission in submissions:
-            sub_list.append(submission)
-        url2 = random.choice(sub_list)
-        url = url2.url
-        await client.send_message(message.channel, url)
-        
-    if message.content.startswith('!getkeem'):
-        import random
-        submissions = r.get_subreddit("KeemGnome").get_top_from_all(limit=100)
-        sub_list = []
-        for submission in submissions:
-            sub_list.append(submission)
-        url2 = random.choice(sub_list)
-        url = url2.url
-        await client.send_message(message.channel, "WHATS UP GUYS IT'S KILLLLLLLEEEEEEEEEERRRRRRRRR KEEEEEEEEEMMMMMMMMSSSSSSTTTTTAAARRRRRRRR" + url)
-        await client.send_message(message.channel, "LEEEETTTTSSS GEEEEETTTT RIIIIGHT INTOOOOO THE NEEEWWWSSSS")
- 
+# TBI Music Bot Code
+
     #if message.content.startswith("!Voice"):
        # global voice
        # channel = client.get_channel("255124792909234176")
--- launch.py
+++ launch.py
@@ -1,48 +1,82 @@
-import time,platform,sys
-from io import open
+import time, platform, sys
 from os.path import isfile
 
-env = {}
-report = []
-rn = str()
-rs = 1
+launch_program = "barry.py"
 
-try:
-    with open("novo.py") as f:
-      code = compile(f.read(), "novo.py", 'exec')
-      exec(code, env)
-except Exception as e:
-    rn = "crash_"+time.strftime("%Y-%m-%d-utc",time.gmtime())
-    print ("[ERROR] It looks like something broke! Generating crash report.")
-    if isfile(rn+".txt"):
-        while isfile(rn+'_'+str(rs)+".txt"):
-            rs += 1
-        rn += '_'+str(rs)
-    rn += ".txt"
+env = {}
+
+def launch(program=launch_program):
+    global env
+    
+    env_l = {}
+    report = str()
+    report_name = str()
+    report_suffix = 1
+    val_container = str()
+    val_multiline = bool()
+    
     try:
-        with open(rn,'w') as f:
-            f.write(u"It looks like something broke!\n\n")
-            try:
-                f.write(u"ver: '"+unicode(env['ver'])+u"'\n\n")
-            except KeyError:
-                f.write(u"ver: undefined\n\n")
-            f.write(  u"platform.platform: '"+unicode(platform.platform())+u"'\n"
-                     +u"platform.python_version: '"+unicode(platform.python_version())+u"'\n"
-                     +u"is_64bits: "+unicode(sys.maxsize > 2**32)+u"\n\n"
-                     +u"e.__class__.__name__: "+unicode(e.__class__.__name__)+u"\n"
-                     +u"e.args: "+unicode(e.args)+"\n\n"                                      )
-            for i in ("__builtins__","password","ver"):
-                try:
-                    del env[i]
-                except KeyError:
-                    pass
-            for i in env:
-                report = unicode(i)+u': '+(u"'"+unicode(env[i])+u"'" if type(env[i]) is str else u"u'"+env[i]+u"'" if type(env[i]) is unicode else unicode(env[i]))+u'\n'
-                if u'\n' in report[:-1]:
-                    report = u'\n'+report+u'\n'
-                f.write(report)
-            f.close()
-    except IOError:
-        print ("[ERROR] Unable to save crash report.")
-    else:
-        print ("[INFO] Crash report saved as '"+rn+"'")
+        with open(launch_program) as f:
+            code = compile(f.read(), program, 'exec')
+            exec(code, env_l)
+    except Exception as e:
+        env = env_l
+        report_name = "crash_" + time.strftime("%Y-%m-%d-utc", time.gmtime())
+
+        print("[ERROR] It looks like something broke! Generating crash report.")
+
+        if isfile(report_name + ".txt"):
+            while isfile(report_name + '_' + str(report_suffix) + ".txt"):
+                report_suffix += 1
+
+            report_name += '_' + str(report_suffix)
+
+        report_name += ".txt"
+
+        try:
+            with open(report_name, 'w', encoding="utf-8") as report_file:
+                report_file.write("It looks like something broke!\n\n")
+
+                if "ver" in env_l:
+                    report_file.write("ver: '" + str(env_l['ver']) + "'\n\n")
+                else:
+                    report_file.write("ver: undefined\n\n")
+
+                report_file.write("platform.platform: '" + str(platform.platform()) + "'\n" +
+                                  "platform.python_version: '" + str(platform.python_version()) + "'\n" +
+                                  "is_64bits: " + str(sys.maxsize > 2**32) + "\n\n" +
+                                  "e.__class__.__name__: " + str(e.__class__.__name__) + "\n" +
+                                  "e.args: " + str(e.args) + "\n\n"                                      )
+
+                for var in ("__builtins__", "ver", "reddit_pass", "discord_key"):
+                    try:
+                        del env_l[var]
+                    except KeyError:
+                        pass
+
+                for var,val in env_l.items():
+                    val_container = ''
+                    val_multiline = False
+                    report = str(var) + ': '
+                    
+                    if type(env_l[var]) is str:
+                        if '\n' in env_l[var]:
+                            val_container = "'''"
+                            val_multiline = True
+                        else:
+                            val_container = "'"
+
+                    report += val_container + val + val_container + '\n'
+
+                    if val_multiline:
+                        report = '\n' + report + '\n'
+
+                    report_file.write(report)
+        except IOError:
+            print ("[ERROR] Unable to save crash report.")
+        else:
+            print ("[INFO] Crash report saved as '" + report_name + "'")
+
+if __name__ in ("__main__", "__builtins__"):
+    while True:
+        launch()
